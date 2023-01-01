e57e6883b2a941baaa74df9b71290bbd8e6856c6
Nicolas Perrier <nicolas.perrier@etu.unilim.fr>
2023-01-01 18:14:06 +0000

    main: initial commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('api')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('users')
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Put('update/:id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete('delete/:id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+
+
+}
